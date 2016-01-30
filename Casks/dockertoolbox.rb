1db7dc2b94ce8f865d880eddbb9c1fd957fc0c31
Pei-Tang Huang <tangtheone@gmail.com>
2016-01-30 13:50:53 +0800

    Updated dockertoolbox to 1.9.1j.

--- Casks/dockertoolbox.rb
+++ Casks/dockertoolbox.rb
@@ -1,10 +1,10 @@
 cask 'dockertoolbox' do
-  version '1.9.1i'
-  sha256 'dc779545acbd2e668cccffb43f0c9933f506ea7b9e8a86bb89a30b0f2d9bd912'
+  version '1.9.1j'
+  sha256 '6ba0a689641dbe8adf86c6f63321f0eff468ae7fa4b2e293850d9ead8e3bf9c4'
 
   url "https://github.com/docker/toolbox/releases/download/v#{version}/DockerToolbox-#{version}.pkg"
   appcast 'https://github.com/docker/toolbox/releases.atom',
-          checkpoint: 'cf821b870418c2de18c13560d79c52d1e57f3379f8dd4729e25ba8509561979d'
+          checkpoint: '8e22b237d98317c6590cc2a8d0cbcb4a29354751e7f69e522b770950e003aa78'
   name 'Docker Toolbox'
   homepage 'https://www.docker.com/toolbox'
   license :apache
